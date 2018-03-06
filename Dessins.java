0c21c1704bfc29bfd42989574084e9d9cbe0ef5a
adonnart <alexandre.donnart@gmail.com>
2018-03-06 16:57:10 +0100

    Add files via upload

---
+++ Dessins.java
@@ -0,0 +1,15 @@
+
+class Dessins {
+	
+    public static void main(String[] args) {
+		
+		System.out.println("\n");
+		System.out.println(" #####   #####   #       #   #     #     # # # \n");
+		System.out.println(" #####   #   #   ##       # #     # #     # #  \n");
+		System.out.println(" #####   #   #   # #       #     #   #   # # # \n");
+		System.out.println(" #####   #   #   #  #     # #     # #     # #  \n");
+		System.out.println(" #####   #####   #####   #   #     #     # # # \n");
+		
+    }
+	
+}
